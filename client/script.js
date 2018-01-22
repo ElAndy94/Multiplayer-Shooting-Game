bbdc5aec52ff4b7b84e03ee1389559051b630cf4
Andy <andrewpeliza@hotmail.com>
2018-01-22 15:33:28 +0000

    Update with sound working, better functionality.

--- client/script.js
+++ client/script.js
@@ -1,23 +1,21 @@
-//var io = require('socket.io')(80);
-// var io = require('socket.io')(server);
-
-
-
-    var WIDTH = 500;
-	  var HEIGHT = 500;
-    // var soundID = "Thunder"; //sound
+var soundID = "Thunder"; //sound
 
     //signIn/Out
     var signDiv = document.getElementById('signDiv'); //<!-- Sign in Div -->
     var signDivUsername = document.getElementById('signDiv-username'); //<!-- username element -->
     var signDivSignIn = document.getElementById('signDiv-signIn'); //<!-- sign in element -->
     var signDivPassword = document.getElementById('signDiv-password'); //<!-- password element -->
+    var signDivUp = document.getElementById('signDiv-signUp'); //<!-- password element -->
 
     signDivSignIn.onclick = function(){  //<!-- clicking the sign in button does this function -->
-        socket.emit('signIn',{username:signDivUsername.value,password:signDivPassword.value}); //<!-- emits username + password to server -->
+        socket.emit('LogIn',{username:signDivUsername.value,password:signDivPassword.value}); //<!-- emits username + password to server -->
         }
 
-    socket.on('signInResponse',function(data){  // Sign in Response function
+    signDivUp.onclick = function(){  //<!-- clicking the sign in button does this function -->
+        socket.emit('signUp',{username:signDivUsername.value,password:signDivPassword.value}); //<!-- emits username + password to server -->
+        }
+
+    socket.on('LogInResponse',function(data){  // Sign in Response function
         if(data.success){
             signDiv.style.display = 'none'; //display none until the logging has been passed
             gameDiv.style.display = 'inline-block'; //when log in has been passed then it will display the gameDiv
@@ -25,88 +23,173 @@
             alert("Sign in was unsuccessful!"); //if password or username is wrong, you get this alart
     });
 
-    //Chat
-    var chatText = document.getElementById('chat-text'); //chat text
-    var chatInput = document.getElementById('chat-input'); //chat input
-    var chatForm = document.getElementById('chat-form'); //the chat form
-
-    socket.on('addToChat',function(data){  //this adds the message to chat
-       chatText.innerHTML += '<div>' + data + '</div>';
+    socket.on('signPlayerRes',function(data){  // Sign UP*** in Response function
+        if(data.success){
+            alert("Sign up was successful!");
+        }  else
+            alert("Sign in was unsuccessful!"); // Not signing up
     });
-
-    socket.on('evalAnswer',function(data){  //evalutation answer
-       console.log(data);
-    });
-
-    chatForm.onsubmit = function(e) {
-        e.preventDefault(); //prevents ""
-        if(chatInput.value[0] === '/')
-		{
-            socket.emit('evalServer',chatInput.value.slice(1)); //to evaluate
-		}
-        else
-            socket.emit('sendMsgToSever',chatInput.value); //this is to send message to sever
-        chatInput.value = ''; //resets ""
-    }
-
 
 
 //game
 
-  // function loadSound () {
-  // createjs.Sound.registerSound("assets/thunder.mp3", soundID);
-  // }
-  //
-  // function playSound () {
-  // createjs.Sound.play(soundID);
-  // }
+  function loadSound () {
+  createjs.Sound.registerSound("/client/shot.mp3", soundID);
+  }
+  
+  function playSound () {
+  createjs.Sound.play(soundID);
+  }
 
   var ctx = document.getElementById('ctx').getContext('2d');
   ctx.font = '30px Arial';
 
+  var Img = {}; //creats image array
+  Img.grass = new Image(); 
+  Img.grass.src = '/client/images/grasses.png'; //loads the background
 
-  socket.on('newPositions', function(data){
-    ctx.clearRect(0,0,500,500);
+  var Player = function(startingPack) {
+      var self = {}
+      self.id = startingPack.id;
+      self.number = startingPack.number;
+      self.x = startingPack.x;
+      self.y = startingPack.y;
+      self.healthPoints = startingPack.healthPoints;
+      self.score = startingPack.score;
+      self.maxHealthPoints = startingPack.maxHealthPoints;
+      Player.list[self.id] = self;
+      return self;
+  }
+  Player.list = {};
+
+  var Bullet = function(startingPack) {
+      var self = {}
+      self.id = startingPack.id;
+      self.number = startingPack.number;
+      self.x = startingPack.x;
+      self.y = startingPack.y;
+      Bullet.list[self.id] = self;
+      return self;
+  }
+  Bullet.list = {};
+
+  //player and bullet info big pack
+socket.on('starterPack',function(data){
     for(var i = 0; i < data.player.length; i++) //player
-        ctx.fillText(data.player[i].number,data.player[i].x,data.player[i].y);
-
+        // ctx.fillRect(data.player[i].x,data.player[i].y,10,10);
+        new Player(data.player[i]);
 
     for(var i = 0; i < data.bullet.length; i++) //bullet
-        ctx.fillRect(data.bullet[i].x-5,data.bullet[i].y-5,10,10);
+        // ctx.fillRect(data.bullet[i].x-1,data.bullet[i].y-1,10,10);
+        new Bullet(data.bullet[i]);
   });
+
+// update player and bullet location - id
+socket.on('update',function(data){
+    for(var i = 0; i < data.player.length; i++){
+        var pack = data.player[i];
+        var ple = Player.list[pack.id];
+        if(ple) {
+                if(pack.x !== undefined)
+                   ple.x = pack.x;
+                if(pack.y !== undefined)
+                   ple.y = pack.y;
+                if(pack.heatlhPoints !== undefined)
+                   ple.healthPoints = pack.healthPoints;
+                if(pack.score !== undefined)
+                   ple.score = pack.score;
+        }
+    }
+
+    for(var i = 0; i < data.bullet.length; i++){
+        var pack = data.bullet[i];
+        var bul = Bullet.list[data.bullet[i].id];
+        if(bul) {
+                if(pack.x !== undefined)
+                   bul.x = pack.x;
+                if(pack.y !== undefined)
+                   bul.y = pack.y;
+        }
+    }
+});
+    
+//remove player/bullet
+socket.on('remove',function(data){
+    for(var i = 0; i < data.player.length; i++){
+        delete Player.list[data.player[i]];
+    }
+    for(var i = 0; i < data.bullet.length; i++){
+        delete Bullet.list[data.bullet[i]];
+    }
+});
+
+//draw
+setInterval(function(){
+    ctx.clearRect(0,0,500,500);
+    drawGrass();
+    for(var i in Player.list)
+        // var healthPointsWidth = 30 * self.healthPoints / self.maxHealthPoints;
+
+        // ctx.fillRect(self.x - healthPointsWidth/2,self.y - 40,healthPointsWidth,4);
+        ctx.fillRect(Player.list[i].x,Player.list[i].y,10,10);
+        // ctx.fillText(self.score,self.x,self.y-60);
+
+    for(var i in Bullet.list)
+        ctx.fillRect(Bullet.list[i].x-1,Bullet.list[i].y-1,10,10);
+},40);
+
+
+
+  var drawGrass = function(){
+		ctx.drawImage(Img.grass,1,1,500,500); //draws the grass in that area
+	}
+
 
   document.onkeydown = function(event){ //keyboard controls
       if(event.keyCode === 39) //right arrow
-          socket.emit('keyPress' ,{inputId:'right',state:true});
+          socket.emit('movementKey' ,{inputId:'right',state:true});
       if(event.keyCode === 40) //down arrow
-          socket.emit('keyPress' ,{inputId:'down',state:true});
+          socket.emit('movementKey' ,{inputId:'down',state:true});
       if(event.keyCode === 37) //left arrow
-          socket.emit('keyPress' ,{inputId:'left',state:true});
+          socket.emit('movementKey' ,{inputId:'left',state:true});
       if(event.keyCode === 38) //up arrow
-          socket.emit('keyPress' ,{inputId:'up',state:true});
+          socket.emit('movementKey' ,{inputId:'up',state:true});
+    //   if(event.keyCode === 32) //space bar
+    //       socket.emit('movementKey' ,{inputId:'attack',state:true});
+    //       playSound();
   }
 
   document.onkeyup = function(event){
       if(event.keyCode === 39) //right arrow
-          socket.emit('keyPress' ,{inputId:'right',state:false});
+          socket.emit('movementKey' ,{inputId:'right',state:false});
       if(event.keyCode === 40) //down arrow
-          socket.emit('keyPress' ,{inputId:'down',state:false});
+          socket.emit('movementKey' ,{inputId:'down',state:false});
       if(event.keyCode === 37) //left arrow
-          socket.emit('keyPress' ,{inputId:'left',state:false});
+          socket.emit('movementKey' ,{inputId:'left',state:false});
       if(event.keyCode === 38) //up arrow
-          socket.emit('keyPress' ,{inputId:'up',state:false});
+          socket.emit('movementKey' ,{inputId:'up',state:false});
+    //   if(event.keyCode === 32) //space bar up
+    //       socket.emit('movementKey' ,{inputId:'attack',state:false});
    }
 
+
    document.onmousedown = function(event){
-     socket.emit('keyPress',{inputId:'attack',state:true});
+     socket.emit('movementKey',{inputId:'attack',state:true}); //mouse down to attack (click)*
+
+     if(event.target.attributes["id"].value !== "ctx"){ // only plays shot sound once im logged in, had clickdown sound when pressing login!*
+         return;
+     } else {
+         playSound();
+     }
    }
+
    document.onmouseup = function(event){
-     socket.emit('keyPress',{inputId:'attack',state:false});
+     socket.emit('movementKey',{inputId:'attack',state:false}); //when not attacking, (unlicked)*
    }
 
    document.onmousemove = function(event){
       var x = -250 + event.clientX - 8;  //500 being the whole screen
       var y = -250 + event.clientY - 8;
       var angle = Math.atan2(y,x) / Math.PI * 180;
-      socket.emit('keyPress',{inputId:'mouseAngle',state:angle});
+      socket.emit('movementKey',{inputId:'mouseAngle',state:angle});
    }
--- shooter.js
+++ shooter.js
@@ -1,7 +1,10 @@
+var mongojs = require("mongojs"); //require mongo js packages
+var db = mongojs('localhost/shooterGame', ['account','score']);
 var express = require('express'); //require the express package
 var app = express();  //use the express package
 var server = require('http').Server(app);  //require http
 
+//db.account.insert({username:"Melissa",password:"Astbury"});
 
 app.get('/',function(req, res){
   res.sendFile(__dirname + '/client/index.html');
@@ -13,56 +16,60 @@ server.listen(8081);   //listens to the localhost:8081
 console.log("Server started.");  //Sends "sever started" to the server, so i can see when ive connected.
 
 var SOCKET_LIST = {};
-//var PLAYER_LIST = {};
 
 var Shared = function(){
     var self = {
-      x:250,
-      y:250,
-      speedX:0,
-      speedY:0,
+      x:Math.random() * 500, // X of the character or bullet
+      y:Math.random() * 500, // Y of the character or bullet
+      speedX:0, //speed X defult to 0
+      speedY:0, //speed Y defult to 0
       id:"",
     }
     self.update = function(){
-        self.updatePos();
+        self.updatePos(); //update char or bullet position
     }
     self.updatePos = function(){
         self.x += self.speedX;
         self.y += self.speedY;
     }
     self.getDist = function(pt){
-        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2)); //calculates distance
     }
     return self;
 }
 
 
 var Player = function(id){
-  var self = Shared();
+  var self = Shared(); //shared properties between bullet and player
     self.id = id;
     self.number = '' + Math.floor(10 * Math.random());
-    self.pRight = false;
-    self.pLeft = false;
-    self.pUp = false;
-    self.pDown = false;
-    self.pAttack = false;
-    self.mouseAngle = 0;
-    self.maxSpeed = 10;
+    self.pRight = false; //moving right auto false
+    self.pLeft = false; //moving left auto false
+    self.pUp = false; //moving up auto false
+    self.pDown = false; //moving down auto false
+    self.pAttack = false; //attacking set to fasle which is shooting!
+    self.mouseAngle = 0; //mouse or touchpad angle
+    self.maxSpeed = 3; //moving speed 10 (need to modify this)***
+    self.healthPoints = 10; // player hp
+    self.maxHealthPoints = 10; // the max hp a player starts with
+    self.score = 0; //score starts at 0, +1 for every kill.
 
+  
     var second_update = self.update;
     self.update = function(){
         self.updateSpeed();
         second_update();
 
-        if(self.pAttack){
+        if(self.pAttack){ 
             self.fireBullet(self.mouseAngle);
         }
   }
   self.fireBullet = function(angle){
-    var b = Bullet(self.id,angle);
+    var b = Bullet(self.id,angle); //bullet id, with angle pack
     b.x = self.x;
     b.y = self.y;
   }
+
 
   self.updateSpeed = function(){
     if(self.pRight)
@@ -70,16 +77,44 @@ var Player = function(id){
     else if(self.pLeft)
           self.speedX = -self.maxSpeed;
     else
-          self.speedX = 0;
+          self.speedX = 0; //reset movement speeds
+          // self.speedY = 0;
 
     if (self.pUp)
           self.speedY = -self.maxSpeed;
     else if(self.pDown)
           self.speedY = self.maxSpeed;
     else
-          self.speedY = 0;
+          self.speedY = 0; //reset movement speeds
+          // self.speedX = 0;
     }
+
+    self.retriveStarterPack = function(){
+      return {
+              id:self.id,
+              x:self.x,
+              y:self.y,
+              number:self.number,
+              healthPoints:self.healthPoints,
+              maxHealthPoints:self.maxHealthPoints,
+              score:self.score,
+            };
+    }
+
+    self.getUpdatePack = function(){
+      return {
+              id:self.id,
+              x:self.x,
+              y:self.y,
+              healthPoints:self.healthPoints,
+              score:self.score,
+            };
+    }
+
     Player.list[id] = self;
+
+    starterPack.player.push(self.retriveStarterPack());
+    
     return self;
 }
 
@@ -88,133 +123,205 @@ Player.onConnect = function(socket){
     var player = Player(socket.id);
     socket.on('movementKey',function(data){
       if(data.inputId === 'left')
-          player.pLeft = data.state;
+          player.pLeft = data.state; //moving left
       else if(data.inputId === 'right')
-          player.pRight = data.state;
+          player.pRight = data.state; //moving right
       else if(data.inputId === 'up')
-          player.pUp = data.state;
+          player.pUp = data.state; //moving up
       else if(data.inputId === 'down')
-          player.pDown = data.state;
+          player.pDown = data.state; //moving down on keyboard
       else if(data.inputId === 'attack')
-          player.pAttack = data.state;
+          player.pAttack = data.state; //if click = true
       else if(data.inputId === 'mouseAngle')
-          player.mouseAngle = data.state;
+          player.mouseAngle = data.state; //mouse angle (direction of shooting)
     });
+
+
+    socket.emit('starter',{
+        player:Player.startPack(),
+        bullet:Bullet.startPack(),
+    })
 }
-Player.onDisconnect = function(socket){
-    delete Player.list[socket.id];
+
+Player.startPack = function(){
+  var players = [];
+  for(var i in Player.list)
+      players.push(Player.list[i].retriveStarterPack());
+      return players;
+}
+
+Player.onDisconnect = function(socket){ 
+    delete Player.list[socket.id]; //delete player from players list
+    deletePack.player.push(socket.id);
 }
 Player.update = function(){
   var pack = [];
   for(var i in Player.list){
     var player = Player.list[i];
-    player.update();
-    pack.push({
-      x:player.x,
-      y:player.y,
-      number:player.number
-    });
+    player.update(); //update player
+    pack.push(player.getUpdatePack());//push player number, x & y
   }
   return pack;
 }
 
-var Bullet = function(parent,angle){
-    var self = Shared();
-    self.id = Math.random();
-    self.speedX = Math.cos(angle/180*Math.PI) * 10;
+var Bullet = function(parent,angle){ //bullet 
+    var self = Shared(); //uses shared properties with player
+    self.id = Math.random(); //random id
+    self.speedX = Math.cos(angle/180*Math.PI) * 10; 
     self.speedY = Math.sin(angle/180*Math.PI) * 10;
-    self.parent = parent;
-    self.timer = 0;
-    self.toRemove = false;
+    self.parent = parent; //so you dont shoot yourself.
+    self.timer = 0; //bullet timer - dies at 100.
+    self.toRemove = false; //if shot yourself then = true.
     var second_update = self.update;
+    
     self.update = function(){
-        if(self.timer++ > 100)
-          self.toRemove = true;
+        if(self.timer++ > 100) //timeout on bullet traveling
+          self.toRemove = true; //removes it
         second_update();
 
         for (var i in Player.list){
           var p = Player.list[i]
-          if(self.getDist(p) < 32 && self.parent !== p.id){
+          if(self.getDist(p) < 32 && self.parent !== p.id){ //gets distance
+            p.healthPoints -= 1;
+
+            if(p.healthPoints <= 0){
+              var enemy = Player.list[self.parent];
+            if(enemy)
+              enemy.score += 1;
+              p.healthPoints = p.maxHealthPoints;
+              p.x = Math.random() * 500;
+              p.y = Math.random() * 500;
+            }
               self.toRemove = true;
           }
         }
     }
+
+    self.retriveStarterPack = function(){
+      return {
+              id:self.id,
+              x:self.x,
+              y:self.y,
+            };
+    }
+
+    self.getUpdatePack = function(){
+      return {
+              id:self.id,
+              x:self.x,
+              y:self.y,
+            };
+    }
+
     Bullet.list[self.id] = self;
+    starterPack.bullet.push(self.retriveStarterPack());
     return self;
 }
-Bullet.list = {};
+Bullet.list = {}; //bullet 
 
-Bullet.update = function(){
+Bullet.update = function(){  //pushes bullet
   var pack = [];
   for(var i in Bullet.list){
     var bullet = Bullet.list[i];
       bullet.update();
-      if(bullet.toRemove)
+      if(bullet.toRemove){
         delete Bullet.list[i]
-      else
-          pack.push({
-          x:bullet.x,
-          y:bullet.y
-      });
+        deletePack.bullet.push(bullet.id);
+      } else
+          pack.push(bullet.getUpdatePack());
   }
   return pack;
 }
 
-var DEBUG = true;
-
-var ONLINEUSERS = {
-  'andy':'peliza',
-  'a':'p',
-  'andrew':'peliza',
+Bullet.startPack = function(){
+  var bullets = [];
+    for(var i in Bullet.list)
+        bullets.push(Bullet.list[i].retriveStarterPack());
+        return bullets;
 }
 
-var isValidPassword = function(data){
-    return ONLINEUSERS[data.username] === data.password;
+
+var isValidPassword = function(data,cb){ //data + call back
+  db.account.find({username:data.username,password:data.password},function(err,res){
+      if(res.length > 0) //if match then its correct! SO LET USER LOG IN!
+        cb(true);
+      else
+        cb(false);
+  });
+}
+
+var takenUser = function(data,cb){
+  db.account.find({username:data.username},function(err,res){
+    if(res.length > 0) //if match then its correct! SO LET USER LOG IN!
+      cb(true);
+    else
+      cb(false);
+});
+}
+
+var addPlayer = function(data,cb){ 
+  db.account.insert({username:data.username,password:data.password},function(err,res){
+    if(res.length > 0) //if match then its correct! SO LET USER LOG IN!
+      cb();
+    });
 }
 
 var io = require('socket.io')(server,{});
 io.sockets.on('connection', function(socket){
-    socket.id = Math.random();
+    socket.id = Math.random(); //adds random socket id to player
     SOCKET_LIST[socket.id] = socket;
 
     socket.on('LogIn',function(data){
-      if(isValidPassword(data)){
-         Player.onConnect(socket);
-         socket.emit('LogInResponse',{success:true});
+      isValidPassword(data,function(res){
+        if(res){
+        Player.onConnect(socket);
+        socket.emit('LogInResponse',{success:true});
       } else {
          socket.emit('LogInResponse',{success:false});
       }
-    })
+    });
+  });
 
+    socket.on('signUp',function(data){
+      takenUser(data,function(res){
+        if(res){
+         socket.emit('signPlayerRes',{success:false});
+      } else {
+        addPlayer(data,function(){
+         socket.emit('signPlayerRes',{success:true});
+        });
+      }
+    });
+  });
 
-    socket.on('disconnect',function(){
+    socket.on('disconnect',function(){ //disconnect by deleting socket id and player
       delete SOCKET_LIST[socket.id];
       Player.onDisconnect(socket);
     });
-    socket.on('sendMsgToSever',function(data){
-      var playerName = ('' + socket.id).slice(2,7);
-      for(var i in SOCKET_LIST) {
-        SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-      }
-    });
-    socket.on('evalServer',function(data){
-      if(!DEBUG)
-              return;
 
-      var res = eval(data);
-      socket.emit('evalAnswer',res);
-    })
 
-});
+ });
+
+var starterPack = {player:[],bullet:[]};
+var deletePack =  {player:[],bullet:[]};
+
 
 setInterval(function(){
   var pack = {
-      player:Player.update(),
-      bullet:Bullet.update(),
+      player:Player.update(), //refreshes player intervals
+      bullet:Bullet.update(), //refreshes bullets intervals
   }
 
   for (var i in SOCKET_LIST){
     var socket = SOCKET_LIST[i];
-    socket.emit('newPos',pack);
+    socket.emit('starterPack',starterPack);
+    socket.emit('update',pack); //emits new positions
+    socket.emit('deletePack',deletePack);
   }
+  starterPack.player = []; //sets everything to 0 so it doesnt repeat / replicate
+  starterPack.bullet = []; //sets everything to 0 so it doesnt repeat / replicate
+  deletePack.player = []; //sets everything to 0 so it doesnt repeat / replicate
+  deletePack.bullet = []; //sets everything to 0 so it doesnt repeat / replicate
+
+
 },1000/25);
